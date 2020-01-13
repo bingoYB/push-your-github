539c9f303002766f969f7cde14b074c6bc019053
bingo <binhdu@foxmail.com>
2020-01-13 17:11:04 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
+import { init, push } from "./git"
 const core = require('@actions/core')
-import { init, push } from "./git";
 /** Initializes and runs the action. */
 (async function () {
   try {
-    await init();
-    await push();
+    await init()
+    await push()
   } catch (error) {
-    console.log("The deployment encountered an error. ❌");
-    core.setFailed(error.message);
+    console.log("The deployment encountered an error. ❌")
+    core.setFailed(error.message)
   } finally {
-    console.log("Completed Deployment ✅");
+    console.log("Completed Deployment ✅")
   }
-})();
+})()
