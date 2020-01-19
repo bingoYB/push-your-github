2c3df9092d8a32e401717aa5db859f36ed437d78
bingo <binhdu@foxmail.com>
2020-01-19 16:16:59 +0800

    update

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 
 const github = require('@actions/github')
 
-const { actor, repository } = github.context.payload
+const { actor } = github.context.payload
 
 const action = {
   gitHubToken: core.getInput("GITHUB_TOKEN"),
@@ -13,6 +13,6 @@ const action = {
 
 exports.action = action
 
-exports.repositoryPath = `https://${actor}:${action.gitHubToken}@github.com/${repository}.git`
+exports.repositoryPath = `https://${actor}:${action.gitHubToken}@github.com/${process.env.GITHUB_REPOSITORY}.git`
 
 exports.workspace = process.env.GITHUB_WORKSPACE;
